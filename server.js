ed9f62050dc2b1bd03f7f50b88821261dce6bbde
bortkyle18 <bortkyle18@gmail.com>
2022-06-26 20:49:01 -0400

    functions done

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// const cTable = require('console.table');
 const figlet = require('figlet');
 const inquirer = require('inquirer');
 const db = require('./db/connection');
@@ -6,10 +5,6 @@ const db = require('./db/connection');
 const viewAllDepartments = require('./FUNC/viewAllDepartments');
 const viewAllRoles = require('./FUNC/viewAllRoles');
 const viewAllEmployees = require('./FUNC/viewAllEmployees');
-// const addDepartment = require('./FUNC/addDepartment');
-// const addRole = require('./FUNC/addRole');
-// const addEmployee = require('./FUNC/addEmployee');
-// const updateEmployeeRole = require('./FUNC/updateEmployeeRole');
 
 
 // Connect to the DB
@@ -48,30 +43,275 @@ function startApp() {
                 startApp();
                 break;
             case 'View All Employees':
-                viewAllEmployees(),
-                startApp();
+                viewAllEmployees();
                 break;
             case 'Add Department':
-                addDepartment(),
-                startApp();
+                addDepartment();
                 break;
             case 'Add Role':
-                addRole(),
-                startApp();
+                addRole();
                 break;
             case 'Add Employee':
-                addEmployee(),
-                startApp();
+                addEmployee();
                 break;
             case "Update Employee's Role":
-                updateEmployeeRole(),
-                startApp();
+                updateEmployeeRole();
                 break;
             default:
-                db.end();
+                endApp();
             }
         })
     .catch(err => {
             console.error(err);
         });
+};
+
+
+// Confirm Quit
+function endApp() {
+    const confirmQuit = [{
+        type: 'list',
+        name: 'quit',
+        message: 'Are you sure you would like to exit Employee Tracker?',
+        loop: false,
+        choices: ['Return to Employee Tracker','Quit']
+    }]
+    
+    inquirer.prompt(confirmQuit)
+    .then(res => {
+        switch (res.quit) {
+            case 'Return to Employee Tracker':
+                startApp();
+            default:
+                db.end();
+        }
+    })
+    .catch(err => {
+        console.error(err);
+    });
+};
+
+
+
+
+
+
+
+
+
+
+
+
+// FUNCTIONS NOT IN FUNC FOLDER
+
+// Add New Department
+const addDepartment = () => {
+    let question = [
+        {
+            type: "input",
+            name: "name",
+            message: "what is the name of the new department?"
+        }
+    ];
+    inquirer.prompt(question)
+    .then(data => {
+        db.query(`INSERT INTO department (name) VALUES (?)`, [data.name], (err, res) => {
+            if (err) throw err;
+            console.log(`Successfully added a ${data.name} department at id ${res.insertId}`);
+            startApp();
+        });
+    })
+    .catch(err => {
+        console.error(err);
+    });
+};
+
+
+
+
+
+// Add New Role
+const addRole = () => {
+    // Get the list of all departments with the department_id for a prompt question list
+    const departments = [];
+    db.query("SELECT * FROM department", (err, res) => {
+        if (err) throw err;
+        res.forEach(dpt => {
+            let dObj = {
+            name: dpt.name,
+            value: dpt.id
+            }
+            departments.push(dObj);
+        });
+    
+        let questions = [
+            {
+            type: "input",
+            name: "title",
+            message: "what is the title of the new role?"
+            },
+            {
+            type: "input",
+            name: "salary",
+            message: "what is the salary of the new role?"
+            },
+            {
+            type: "list",
+            name: "department",
+            choices: departments,
+            message: "which department is this role in?"
+            }
+        ];
+
+        inquirer.prompt(questions)
+        .then(data => {
+            const query = `INSERT INTO ROLE (title, salary, department_id) VALUES (?)`;
+            db.query(query, [[data.title, data.salary, data.department]], (err, res) => {
+            if (err) throw err;
+            console.log(`Successfully inserted ${data.title} role at id ${res.insertId}`);
+            startApp();
+            });
+        })
+        .catch(err => {
+            console.error(err);
+        });
+    });
+};
+
+
+
+
+// Add Emplyee
+const addEmployee = () => {
+    //get all the employee list to make choice of employee's manager
+    db.query("SELECT * FROM EMPLOYEE", (err, emplRes) => {
+        if (err) throw err;
+        const employeeChoice = [
+            // No Manager
+            {
+            name: 'None',
+            value: 0
+            }
+        ];
+        emplRes.forEach(({ first_name, last_name, id }) => {
+            employeeChoice.push({
+            name: first_name + " " + last_name,
+            value: id
+            });
+        });
+        
+        //get all the role list to make choice of employee's role
+        db.query("SELECT * FROM ROLE", (err, rolRes) => {
+            if (err) throw err;
+            const roleChoice = [];
+            rolRes.forEach(({ title, id }) => {
+            roleChoice.push({
+                name: title,
+                value: id
+                });
+            });
+        
+            let questions = [
+            {
+                type: "input",
+                name: "first_name",
+                message: "What is the employee's first name?"
+            },
+            {
+                type: "input",
+                name: "last_name",
+                message: "What is the employee's last name?"
+            },
+            {
+                type: "list",
+                name: "role_id",
+                choices: roleChoice,
+                message: "What is the employee's role?"
+            },
+            {
+                type: "list",
+                name: "manager_id",
+                choices: employeeChoice,
+                message: "Who is the employee's manager? (if they have one)"
+            }
+            ]
+        
+            inquirer.prompt(questions)
+            .then(data => {
+                const query = `INSERT INTO EMPLOYEE (first_name, last_name, role_id, manager_id) VALUES (?)`;
+                let manager_id = data.manager_id !== 0? data.manager_id: null;
+                db.query(query, [[data.first_name, data.last_name, data.role_id, manager_id]], (err, res) => {
+                if (err) throw err;
+                console.log(`Successfully inserted employee ${data.first_name} ${data.last_name} with id ${res.insertId}`);
+                startApp();
+                });
+            })
+            .catch(err => {
+                console.error(err);
+            });
+        })
+    });
+};
+
+
+
+
+// Update Employee's Role
+const updateEmployeeRole = () => {
+    //get all the employee list 
+    db.query("SELECT * FROM EMPLOYEE", (err, emplRes) => {
+        if (err) throw err;
+        const employeeChoice = [];
+        emplRes.forEach(({ first_name, last_name, id }) => {
+            employeeChoice.push({
+            name: first_name + " " + last_name,
+            value: id
+            });
+        });
+        
+        //get all the role list to make choice of employee's role
+        db.query("SELECT * FROM ROLE", (err, rolRes) => {
+            if (err) throw err;
+            const roleChoice = [];
+            rolRes.forEach(({ title, id }) => {
+                roleChoice.push({
+                    name: title,
+                    value: id
+                    });
+            });
+        
+            let questions = [
+                {
+                    type: "list",
+                    name: "id",
+                    choices: employeeChoice,
+                    message: "Whose role do you want to update?"
+                },
+                {
+                    type: "list",
+                    name: "role_id",
+                    choices: roleChoice,
+                    message: "what is the employee's new role?"
+                }
+            ]
+        
+            inquirer.prompt(questions)
+            .then(data => {
+                const query = `UPDATE EMPLOYEE SET ? WHERE ?? = ?;`;
+                db.query(query, [
+                {role_id: data.role_id},
+                "id",
+                data.id
+                ], (err, res) => {
+                if (err) throw err;
+                
+                console.log("Successfully updated employee's role!");
+                startApp();
+                });
+            })
+            .catch(err => {
+                console.error(err);
+            });
+        })
+    });
 };
